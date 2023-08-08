097de1a95222ab47041c2398a29f927392dd2793
jeon su jung <sj1910924@icloud.com>
2023-08-08 19:52:54 +0900

    Feat: Footer 추가

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { FaGithub, FaFigma } from "react-icons/fa";
+import { SiNotion } from "react-icons/si";
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-gray-200 text-gray-700 text-center py-2">
+      <p>&copy; 2023 nbc 소고기 육전 All rights reserved.</p>
 
-export default Footer
+      <div className="flex justify-center space-x-4 mt-2">
+        <a href="https://github.com/kimjinsu0210/learntime">
+          <FaGithub size="30" color="#000000" />
+        </a>
+        <a href="https://www.figma.com/file/Z7JOaHPjk2P9OFT30aHYza?embed_host=notion&kind=&mode=design&node-id=0%3A1&t=xfAt3aN2MrMpyTDC-1&type=design&viewer=1">
+          <FaFigma size="30" color="#000000" />
+        </a>
+        <a href="https://www.notion.so/2c8f3970ba3446f08280fe1b09d7c740">
+          <SiNotion size="30" color="#000000" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
