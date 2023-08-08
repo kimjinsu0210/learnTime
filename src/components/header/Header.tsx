23ffc972c8a8a5388f4d1fe7ebfa16a346d5b93b
jeon su jung <sj1910924@icloud.com>
2023-08-08 20:20:40 +0900

    Feat: Header 기본 틀 작성

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BiMessageSquareError } from "react-icons/bi";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-200 text-gray-700 flex items-center justify-between px-4 py-2">
+      <div className="flex items-center">
+        <Link to="/" className="flex items-center">
+          <BiMessageSquareError size="24" color="red" />
+          <h1 className="text-xl ml-2">런타임</h1>
+        </Link>
+      </div>
+      <div className="flex items-center">로그인 회원가입</div>
+    </header>
+  );
 };
 
 export default Header;
