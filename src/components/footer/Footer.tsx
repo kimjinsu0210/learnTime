c4dfd475c251a5b235429640c7138e59ad0e37b0
Jinsu Kim <kimjinsu0210@naver.com>
2023-08-10 10:53:54 +0900

    Feat: 회원가입 닉네임, 프로필 이미지 추가

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,10 +4,10 @@ import { SiNotion } from "react-icons/si";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-200 text-gray-700 text-center py-2">
+    <footer className="fixed bottom-0 w-full py-2 text-center text-gray-700 bg-gray-200">
       <p>&copy; 2023 nbc 소고기 육전 All rights reserved.</p>
 
-      <div className="flex justify-center space-x-4 mt-2">
+      <div className="flex justify-center mt-2 space-x-4">
         <a href="https://github.com/kimjinsu0210/learntime">
           <FaGithub size="30" color="#000000" />
         </a>
