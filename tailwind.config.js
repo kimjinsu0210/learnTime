1e7bf00bc7ed8e5607d1ec47e1fb98333a18dfae
YJ Baek <yjbaek29@gmail.com>
2023-08-10 09:37:20 +0900

    Feat: Main Category List Style: tailwind config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    // added container option center
+    container: {
+      center: true
+    },
     extend: {
+      // added primary color red
+      colors: {
+        primary: "#DF0000",
+        yellow: {
+          450: "#FAD12E"
+        }
+      }
     }
   },
   plugins: []
